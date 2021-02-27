584d52c20b4e09d39524f2e0bbbb3cee588b7f6d
Kale Kundert <kale@thekunderts.net>
2021-02-27 13:44:18 -0500

    test: modernize how pytest collects demos

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,19 +9,19 @@ def pytest_addoption(parser):
 def pytest_collect_file(path, parent):
     if parent.config.option.run_demos:
         if path.fnmatch('demo_*.py'):
-            return DemoFile(path, parent)
+            return DemoFile.from_parent(parent, fspath=path)
 
 class DemoFile(pytest.File):
 
     def collect(self):
         name = self.fspath.basename[:-len('.py')]
-        yield DemoItem(name, self, self.fspath)
+        yield DemoItem.from_parent(self, name=name, fspath=self.fspath)
 
 class DemoItem(pytest.Item):
 
-    def __init__(self, name, parent, path):
+    def __init__(self, name, parent, fspath):
         super().__init__(name, parent)
-        self.path = path
+        self.path = fspath
 
     def runtest(self):
         run_demo(self.path.strpath)
